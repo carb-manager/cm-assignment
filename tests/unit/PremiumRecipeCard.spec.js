c00093e6e333fa92f3e8bdcc8e535992620c3f98
Vinoth Michael Xavier <vinoth.michaelxavier@gmail.com>
2020-03-27 21:32:25 -0400

    Failing test

--- tests/unit/PremiumRecipeCard.spec.js
+++ tests/unit/PremiumRecipeCard.spec.js
@@ -1,3 +1,12 @@
-describe("PremiumRecipeCard.vue", () => {
-  /* Tests go here */
-});
+import { shallowMount } from '@vue/test-utils'
+import PremiumRecipeCard from '@/components/PremiumRecipeCard.vue'
+
+describe('PremiumRecipeCard.vue', () => {
+
+  it('component does render', () => {
+    const wrapper = shallowMount(PremiumRecipeCard)
+    expect(
+      wrapper.attributes()['data-test-component']
+    ).toBe('PremiumRecipeCard')
+  })
+})
