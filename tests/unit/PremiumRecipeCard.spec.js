764d12594a2235ad8bdc8ba1a149169206b02cc6
Vinoth Michael Xavier <vinoth.michaelxavier@gmail.com>
2020-03-28 13:04:37 -0400

    assert DOM classes, create basic DOM structure

--- tests/unit/PremiumRecipeCard.spec.js
+++ tests/unit/PremiumRecipeCard.spec.js
@@ -5,8 +5,20 @@ describe('PremiumRecipeCard.vue', () => {
 
   it('component does render', () => {
     const wrapper = shallowMount(PremiumRecipeCard)
-    expect(
-      wrapper.attributes()['data-test-component']
-    ).toBe('PremiumRecipeCard')
+    expect(wrapper.attributes('class')).toBe('prc')
+
+    // Cover
+    expect(wrapper.find('.prc__cover').exists()).toBe(true)
+    expect(wrapper.find('.prc__img').exists()).toBe(true)
+    expect(wrapper.find('.prc__favorite').exists()).toBe(true)
+    expect(wrapper.find('.prc__tag').exists()).toBe(true)
+
+    // Info
+    expect(wrapper.find('.prc__info').exists()).toBe(true)
+    expect(wrapper.find('.prc__header').exists()).toBe(true)
+    expect(wrapper.find('.prc__ratings').exists()).toBe(true)
+    expect(wrapper.find('.prc__duration').exists()).toBe(true)
+    expect(wrapper.find('.prc__burn').exists()).toBe(true)
+    expect(wrapper.find('.prc__macros').exists()).toBe(true)
   })
 })
