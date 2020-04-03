05c681d628d82ce45bf381b52792040135e42c4e
Jonathan Matthey <mattheyj@gmail.com>
2020-04-03 01:26:56 +0100

    clean up test

--- tests/unit/MacroDots.spec.js
+++ tests/unit/MacroDots.spec.js
@@ -3,10 +3,7 @@ import MacroDots from '@/components/subcomponents/MacroDots'
 
 describe("MacroDots.vue", () => {
   it('shows macros in the correct order, carbs, protein, fats', () => {
-    let wrapper = shallowMount(MacroDots, {
-      propsData: {
-      } 
-    })
+    let wrapper = shallowMount(MacroDots, {})
     expect(wrapper.findAll('.macro-dots span').at(0).classes()).toContain('macro-dot--carbs')
     expect(wrapper.findAll('.macro-dots span').at(1).classes()).toContain('macro-dot--protein')
     expect(wrapper.findAll('.macro-dots span').at(2).classes()).toContain('macro-dot--fats')
