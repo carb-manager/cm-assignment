21c46bc29c9b19432074e43aadfadb6e5b173510
Hoang Nguyen <hoang.developer.remotely@gmail.com>
2020-04-04 11:27:25 +0700

    initialize premium card component

--- src/components/PremiumRecipeCard.vue
+++ src/components/PremiumRecipeCard.vue
@@ -1,3 +1,41 @@
 <template>
-  <div>Premium Recipe Card Component Goes Here</div>
+  <div class="card">
+    <div class="image">
+      <div class="favorite"></div>
+      <div class="badge"></div>
+    </div>
+    <div class="details">
+      <h3 class="title"></h3>
+      <div class="description"></div>
+    </div>
+  </div>
 </template>
+
+<script>
+export default {
+  name: "PremiumRecipeCard",
+  props: {
+    title: {
+      type: String,
+      default: "Premium Recipe Title"
+    },
+    ratings: Number,
+    ratingsCount: Number,
+    isFavorite: {
+      type: Boolean,
+      default: false
+    },
+    imageUrl: {
+      type: String,
+      default: ""
+    },
+    durations: Number,
+    carbs: Number,
+    protein: Number,
+    fats: Number,
+    calories: Number
+  },
+  computed: {},
+  methods: {}
+};
+</script>
