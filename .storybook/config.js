73e76d1832de0e4c60b575478dc5643928ad2448
Jon Preece <jonpreece@hotmail.co.uk>
2020-04-05 19:29:23 +0100

    fix(responsive): minor responsive fixes

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,1 +1,10 @@
+import { addParameters } from "@storybook/vue";
+import { INITIAL_VIEWPORTS } from "@storybook/addon-viewport";
+
 import "../src/styles/cm.css";
+
+addParameters({
+  viewport: {
+    viewports: INITIAL_VIEWPORTS
+  }
+});
