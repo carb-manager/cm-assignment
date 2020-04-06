d3e522f1f21c209fbaa42bf20e10db1b6ecfdd38
Hoang Nguyen <hoang.developer.remotely@gmail.com>
2020-04-07 01:11:36 +0700

    fix favorite component

--- src/components/ui/Favorite.vue
+++ src/components/ui/Favorite.vue
@@ -2,7 +2,6 @@
   <div class="favorite">
     <img v-if="isFavorite" class="dot" src="../../assets/favorite-selected.svg" />
     <img v-if="!isFavorite" class="dot" src="../../assets/favorite.svg" />
-    <img class="icon" src="../../assets/energy.svg" v-if="isShowedIcon" />
   </div>
 </template>
 
